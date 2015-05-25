875e9443f764a0192161bfb78bea012545d44915
6655449987 <6655449987qq@gmail.com>
2015-05-25 23:31:20 +0800

    service injection

--- web/src/main/java/org/company/web/angular/TestController.java
+++ web/src/main/java/org/company/web/angular/TestController.java
@@ -1,14 +1,9 @@
 package org.company.web.angular;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.company.core.common.comstatic.ConfigStatic;
-import org.company.core.common.comstatic.StaticValue;
-import org.company.core.moduel.domain.TestTable;
 import org.company.core.moduel.service.TestTableService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
