c61a40c26b508642a8546e8e710e472786ae8d4b
6655449987 <6655449987qq@gmail.com>
2015-05-25 22:38:03 +0800

    angularJs Basic index

--- web/src/main/webapp/resources/web/index.js
+++ web/src/main/webapp/resources/web/index.js
@@ -1,43 +1,20 @@
 var indexApp = angular.module('indexApp', []);
-indexApp.controller('IndexController',function($scope) {
+indexApp.controller('IndexController',function($scope, $http) {
+	
 	$scope.user="Angular";
 	$scope.sb = {name:'name',gender:'male',age:28};
+	$scope.list = [{name:'name1',gender:'male',age:28},{name:'name2',gender:'female',age:30},{name:'name3',gender:'female',age:10},{name:'name4',gender:'male',age:5}];
+	$scope.url = '/web/test/api/getUser';
+	
 	$scope.sayHello=function(){
-		alert('Hello!'+$scope.sb.name);
+		alert('Hello! \n$scope.sb.name:'+$scope.sb.name+";\nyourname:"+$scope.yourname);
 	};
+	
+
+//	 $http.get('/web/test/api/getUser').success(function(data) {
+//		 	console.log(data);
+//		    $scope.valueViaHttp = data;
+//	 });
 });
 
-indexApp.directive("ezNamecard",function($scope){
-	return {
-		restrict : "E",
-		template : "<div></div>",
-		link : function(scope,element,attrs){
-			element.append("<div>name : <span class='name'></span></div>")
-				.append("<div>gender : <span field='gender'></span></div>")
-				.append("<div>age : <span field='age'></span></div>");
-			scope.$watch(attrs.data,function(nv,ov){
-				var fields = element.find("span");
-				fields[0].innerText = nv.name;
-				fields[1].innerText = nv.gender;
-				fields[2].innerText = nv.age;
-			},true);
-		}
-	};
-});
-indexApp.directive("ezNamecardEditor",function($scope){
-	return {
-		restrict : "E",
-		template : "<div></div>",
-		link : function(scope,element,attrs){
-			var model = attrs.data;
-			element.append("<div>name : <input type='text' field='name'></div>")
-				.append("<div>gender : <input type='text' field='gender'></div>")
-				.append("<div>age : <input type='text' field='age'></div>");
-			element.find("input").on("keyup",function(ev){
-				var field = ev.target.getAttribute("field");
-				scope[model][field] = ev.target.value;
-				scope.$apply("");
-			});
-		}
-	};
-});
+
